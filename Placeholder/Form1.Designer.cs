59967cd66f893152b93915c595477171417db1b5
CppKotlin <cscppkotlin@gmail.com>
2024-05-04 21:25:29 +0300

    1 метод

--- Placeholder/Form1.Designer.cs
+++ Placeholder/Form1.Designer.cs
@@ -28,12 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(776, 27);
+            textBox1.TabIndex = 1;
+            textBox1.Enter += textBox1_Enter;
+            textBox1.Leave += textBox1_Leave;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 79);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(776, 27);
+            textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
--- Placeholder/Form1.cs
+++ Placeholder/Form1.cs
@@ -2,9 +2,36 @@ namespace Placeholder
 {
     public partial class Form1 : Form
     {
+        private string placeholderText = "¬ведите логин";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Gray;
+            textBox1.Text = placeholderText;
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == placeholderText)
+            {
+                textBox1.Text = string.Empty;
+                textBox1.ForeColor = Color.Black;
+
+            }
+
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if(textBox1.Text == string.Empty) 
+            {
+                textBox1.ForeColor = Color.Gray;
+                textBox1.Text = placeholderText;
+            }
         }
     }
 }
